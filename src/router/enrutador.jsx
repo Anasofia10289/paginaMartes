ce333f2b17e09d159a1ab345623c231fff7ee600
carlosurango03 <carlosaoviedo03@gmail.com>
2025-04-01 09:08:43 -0500

    Se crea el login

---
+++ src/router/enrutador.jsx
@@ -0,0 +1,16 @@
+import Login from '../pages/Login'
+import Home from '../Home'
+
+export let enrutador = [
+    {
+        path: '/',
+        element: <Login/>
+    },
+    {
+        path:'/home',   
+        element: <Home/>
+    },
+]
+
+
+
